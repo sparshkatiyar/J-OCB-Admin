f932ab99df649321b5a5e0bb0d491eb75571fd7b
Dheeraj <sanjayica123@321>
2023-11-09 18:24:45 +0530

    tab

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserMgmtComponent } from './user-mgmt/user-mgmt.component';
+import { TableTabMgmtComponent } from './table-tab-mgmt/table-tab-mgmt.component';
 
 const routes: Routes = [
   {path:'', component:LayoutComponent, children:[
     {path:'',redirectTo:'dashboard',pathMatch:'full'},
     {path:'dashboard', component:DashboardComponent},
     {path:'user-mgmt', component:UserMgmtComponent},
+    {path:'table-tab-mgmt', component:TableTabMgmtComponent},
+
 
   ]}
 ];
